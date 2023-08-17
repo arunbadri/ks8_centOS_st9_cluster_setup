ad1fe711b7e5698db600ff3defbe9945033bffb8
arun.badri@outlook.com <arun.badri@outlook.com>
2023-08-17 15:54:56 +0100

    update configs3

--- configs/join_master.sh
+++
@@ -1,1 +0,0 @@
-kubeadm join 10.1.0.5:6443 --token t5alxa.k2cq3kk5nbx3pkwi --discovery-token-ca-cert-hash sha256:b81de30c467b885907da0be3716d03869b26324d5ce3cf2edecc6c59dd2bdd6a --control-plane --certificate-key 1ee7475ce9054d6f40c733d9ec8292e19c22e5a22dd8392f326e816641d8398b --apiserver-advertise-address=$1
